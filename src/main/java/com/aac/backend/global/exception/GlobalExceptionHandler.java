f8da05d0548f920be92ff9ef7ffd20110d89408b
jumdo12 <jumdo12@gmail.com>
2026-05-18 21:54:41 +0900

    fix: www 도메인 허용 및 500 로깅 추가 (#44)

--- src/main/java/com/aac/backend/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/aac/backend/global/exception/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.aac.backend.global.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -22,6 +24,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error("Unhandled exception occurred", e);
         return ResponseEntity.internalServerError().body(ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR));
     }
 }
